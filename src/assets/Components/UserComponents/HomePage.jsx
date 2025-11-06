93885c90e3c3274cbb320cfc2378f4953ddd5a38
Mokosseakowo Ezechiel <mokosseakowoe@gmail.com>
2025-11-06 07:52:21 +0100

    malin

--- src/assets/Components/UserComponents/HomePage.jsx
+++ src/assets/Components/UserComponents/HomePage.jsx
@@ -4,7 +4,7 @@ import { FaCalendarDay, FaArrowCircleDown, FaLightbulb, FaClock, FaBalanceScale 
 
 export default function HomePage() {
   return (
-    <div className="bg-[url('/blog-3.jpg')] bg-cover bg-center md:py-16 py-20  h-screen lg:px-20 md:px-10 px-8 bg-fixed ">
+    <div className="bg-[url('/blog-3.jpg')] bg-cover bg-center md:py-16 py-20  h-screen lg:px-20 md:px-10 px-8 md:bg-fixed ">
       <h1 className='text-center text-white md:text-3xl text-2xl font-bold'>Votre Cabinet de confiance</h1>
       <h1 className='text-center text-blue-600 md:text-4xl text-lg font-bold'>PABINGUI & Associés</h1>
       <p className='text-center font-bold text-white md:text-md text-sm mb-6'>Expertises juridiques <span className='text-xl text-blue-500'>.</span> Conseil personnalisé <span className='text-xl text-blue-500'> .</span> Défense de vos droits</p>
@@ -38,14 +38,14 @@ export default function HomePage() {
             <h1 className='text-xl font-bold text-center'>{new Date().getFullYear() - 2000}+ Ans d'expérience</h1>
             <span className='text-center'>Une expérience reconnue dans tous les domaines du droit</span>
         </div>
-        <div className='p-6 bg-white/20 backdrop-blur-0 rounded-md md:block hidden text-center '>
+        <div className='p-6 bg-white/20 backdrop-blur-0 rounded-md lg:block hidden text-center '>
             <div className='flex justify-center'>
                <FaBalanceScale className='text-4xl p-2 rounded-full bg-blue-600'/>
             </div>
             <h1 className='text-xl font-bold text-center'>Plus de 500 clients satisfaits</h1>
             <span className='text-center'>Une relation de conficiance établie avec nos clients</span>
         </div>
-        <div className='p-6 bg-white/20 backdrop-blur-0 rounded-md text-center md:block hidden'>
+        <div className='p-6 bg-white/20 backdrop-blur-0 rounded-md text-center lg:block hidden'>
             <div className='flex justify-center'>
                <FaClock className='text-4xl p-2 rounded-full bg-blue-600'/>
             </div>
